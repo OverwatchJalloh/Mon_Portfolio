91bf0b7d96a74e48b84fe2aa061fdc4af7fb28b1
Mamadou Moussa Diallo <moussadiallo10.mmd@gmail.com>
2023-11-05 15:54:22 -0500

    contact_form_rectification

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(function () {
-    $(".navbar a, footer a").on("click", function(event){
+    /* $(".navbar a, footer a").on("click", function(event){
         event.preventDefault();
         var hash = this.hash;
 
         $('body').animate({scrollTop: $(hash).offset().top}, 900, function(){window.location.hash = hash;})
-    });
+    }); */
 
     $('#contact-form').submit(function(e){
 
